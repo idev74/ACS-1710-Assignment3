0c1642c7e1d59ade262ccbc969ac99a67081c981
Isabella Devassy <77900249+idev74@users.noreply.github.com>
2022-11-30 21:45:46 -0800

    Image Filtering

--- app.py
+++ app.py
@@ -148,29 +148,31 @@ def apply_filter(file_path, filter_name):
 @app.route('/image_filter', methods=['GET', 'POST'])
 def image_filter():
     """Filter an image uploaded by the user, using the Pillow library."""
-    filter_types = filter_types_dict.keys()
+    filter_type = filter_types_dict.keys()
 
     if request.method == 'POST':
         
         # TODO: Get the user's chosen filter type (whichever one they chose in the form) and save
         # as a variable
         # HINT: remember that we're working with a POST route here so which requests function would you use?
-        filter_type = ''
+        filter_type = request.form.get('filter_type')
         
         # Get the image file submitted by the user
         image = request.files.get('users_image')
 
         # TODO: call `save_image()` on the image & the user's chosen filter type, save the returned
         # value as the new file path
-
+        file_path = save_image(image, filter_type)
         # TODO: Call `apply_filter()` on the file path & filter type
-
+        apply_filter(file_path, filter_type)
         image_url = f'./static/images/{image.filename}'
 
         context = {
             # TODO: Add context variables here for:
             # - The full list of filter types
             # - The image URL
+            'filter_types_dict': filter_types_dict,
+            'image_url': image_url
         }
 
         return render_template('image_filter.html', **context)
@@ -178,7 +180,9 @@ def image_filter():
     else: # if it's a GET request
         context = {
             # TODO: Add context variable here for the full list of filter types
+            'filter_types_dict': filter_types_dict
         }
+
         return render_template('image_filter.html', **context)
 
 
